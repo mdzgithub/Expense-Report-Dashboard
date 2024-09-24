642c2f2add79a79a4b6d887954c37cde8567d8aa
Mark Zindren <113194059+mdzgithub@users.noreply.github.com>
2024-09-24 00:40:01 +0000

    Added csv for local testing and began changing framework

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,28 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.title("🎈 New Title")
+
+st.set_page_config(
+    page_title="Expense Dashboard",
+    page_icon="🎈",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+st.title("🎈 Expense Dashboard")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = pd.read_csv('/workspaces/Expense-Report-Dashboard/expense-report-2024-09-23T20-59-19Z.csv')
+
+st.dataframe(df)
+
+
+st.bar_chart(df[['Date', 'Category', 'Amount']],
+             x='Category',
+             y='Amount')
+
+st.bar_chart(df[['Date', 'Name', 'Amount']],
+             x='Name',
+             y='Amount')
